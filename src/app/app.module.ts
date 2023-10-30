65a9d1334bc555cd1afd47844f35fa882e2d6de5
Juan Sanchez <js.sanchezl1@uniandes.edu.co>
2023-10-29 19:35:40 -0500

    :)

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,24 +1,22 @@
-import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { BookModule } from './book/book.module';
-import { EditorialModule } from './editorial/editorial.module';
+import { SerieModule } from './serie/serie.module';
 
 @NgModule({
- declarations: [
-   AppComponent
- ],
- imports: [
-   BrowserModule,
-   AppRoutingModule,
-   BookModule,
-   EditorialModule,
-   HttpClientModule
- ],
- providers: [],
- bootstrap: [AppComponent]
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    HttpClientModule,
+    SerieModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
 })
 export class AppModule { }
